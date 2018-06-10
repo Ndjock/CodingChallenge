28c4342b81d3db13bee95f2526f4f8fb3f03e64c
arthur <39818751+Ndjock@users.noreply.github.com>
2018-06-10 21:25:23 +0200

    mvc unit test for contact controllers (get)
    Mvc unit test for:
    
    - get Contact  by id ( Get /contacts/id )
    - get Contact  List ( Get /contacts/ )
    -  update Contact by id ( PUT /contacts/id)

--- src/test/java/openwt/interview/coding/challenge/test/web/controllers/WhenAccessingContractControllerEndPoint.java
+++ src/test/java/openwt/interview/coding/challenge/test/web/controllers/WhenAccessingContractControllerEndPoint.java
@@ -1,8 +1,16 @@
 package openwt.interview.coding.challenge.test.web.controllers;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +18,7 @@ import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,7 +28,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -31,7 +44,8 @@ import openwt.interview.coding.challenge.web.controllers.ContactController;
 @RunWith(SpringRunner.class)
 @WebMvcTest(ContactController.class)
 @WithMockUser
-@EnableSpringDataWebSupport 
+@EnableSpringDataWebSupport
+@PrepareForTest
 public class WhenAccessingContractControllerEndPoint {
 
 	@Autowired
@@ -42,6 +56,19 @@ public class WhenAccessingContractControllerEndPoint {
 	@MockBean(name="skillRepository")
 	private SkillRepository skillRepository;
 	
+	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+	
+	@Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+            .findAny()
+            .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
 	
 	@Test
 	public void getRequestForContactSearchByIdShouldReturn200IfContactExists() throws Exception {
@@ -86,4 +113,328 @@ public class WhenAccessingContractControllerEndPoint {
 			.perform(get("/contacts").accept(MediaType.APPLICATION_JSON_UTF8))
 			.andExpect(status().isOk());
 	}
+
+	@Test
+	public void getRequestForContactsListShouldReturnExpectedPagination() throws Exception {
+		
+		// defining init params
+		int contactCount = 10;
+		int pageLength = 4;
+		List<Contact> contacts = new ArrayList<>();
+		// adding contactCount amount of contacts
+		for (int i = 0; i < contactCount ;i++)
+			contacts.add(new Contact());
+
+		//mocking contactPage as the Nth. page of contacts of size x
+		int currentPage = 1; 
+		Page<Contact> contactPage =  new PageImpl<Contact>(contacts.subList(pageLength-1, pageLength),
+										PageRequest.of(currentPage, pageLength),contactCount);
+		Mockito
+			.when(contactRepository.findAll(Mockito.any(Pageable.class)))
+			.thenReturn(contactPage);
+		
+		// assert pagination status 
+		mockMvc
+			.perform(get("/contacts").accept(MediaType.APPLICATION_JSON_UTF8))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$..page.number").value(currentPage)) // second page
+			.andExpect(jsonPath("$..page.size").value(pageLength)) // page.size
+			.andExpect(jsonPath("$..page.totalPages")
+						.value((contactCount/pageLength) + (contactCount%pageLength!=0? 1: 0)));// total pages;
+	}
+	@Test
+	public void getRequestForContactListShouldReturnExceptedContactCount() throws Exception {
+		// defining init params
+		int contactCount = 10;
+		int pageLength = 4;
+		List<Contact> contacts = new ArrayList<>();
+		// adding contactCount amount of contacts
+		for (int i = 0; i < contactCount ;i++)
+			contacts.add(new Contact());
+		
+		Page<Contact> contactPage =  new PageImpl<Contact>(contacts.subList(0, pageLength),
+				PageRequest.of(0, pageLength),contactCount);
+		Mockito
+			.when(contactRepository.findAll(Mockito.any(Pageable.class)))
+			.thenReturn(contactPage);
+		mockMvc.perform(get("/contacts").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$..contactList.length()").value(pageLength));
+	}
+
+	@Test
+	public void postRequestForAddingContactShouldReturn200IfContactIsValid() throws Exception {
+		Contact contact = getValidContact();
+		mockMvc.perform(post("/contacts/")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(contact)))
+				.andExpect(status().isCreated());
+		Mockito.verify(contactRepository).save(Mockito.any(Contact.class));
+
+	}
+
+	private Contact getValidContact() {
+		// Valid contact;
+		Contact contact = new Contact();
+		contact.setFirstname("sadina");
+		contact.setLastname("sadina");
+		contact.setFullname("sadina, sadina");
+		contact.setEmail("saba@saba.com");
+		contact.setPhoneNumber("6219036402");
+		contact.setAddressLine("uipstrasse 0, 91524 fmgqjcat");
+		return contact;
+	}
+
+	
+	private Contact getInvalidContact() {
+		// invalid contact;
+		Contact contact = new Contact();
+		contact.setFirstname(null);
+		contact.setLastname(null);
+		contact.setFullname(null);
+		contact.setEmail(null);
+		contact.setPhoneNumber(null);
+		contact.setAddressLine(null);
+		return contact;
+	}
+
+	
+	@Test
+	public void postRequestForAddingContactShouldReturn400IfFirstnameIsNullOrInvalid() throws Exception {
+		// Valid contact;
+		Contact contact = new Contact();
+		contact.setFirstname(null);
+		contact.setLastname("sadina");
+		contact.setFullname("sadina, sadina");
+		contact.setEmail("saba@saba.com");
+		contact.setPhoneNumber("6219036402");
+		contact.setAddressLine("uipstrasse 0, 91524 fmgqjcat");
+		Mockito.when(contactRepository.save(Mockito.any(Contact.class)))
+			.thenReturn(null);
+
+		mockMvc.perform(post("/contacts/")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(contact)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+				.andExpect(jsonPath("$..details").value(hasItem(containsString("firstname"))));
+		
+		contact.setFirstname("asdadh2");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("firstname"))));
+		
+		contact.setFirstname("  ");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("firstname"))));
+	
+	}
+	
+	@Test
+	public void postRequestForAddingContactShouldReturn400IfLastnameIsNullOrInvalid() throws Exception {
+		// Valid contact;
+		Contact contact = new Contact();
+		contact.setFirstname("sadina");
+		contact.setLastname(null);
+		contact.setFullname("sadina, sadina");
+		contact.setEmail("saba@saba.com");
+		contact.setPhoneNumber("6219036402");
+		contact.setAddressLine("uipstrasse 0, 91524 fmgqjcat");
+		Mockito.when(contactRepository.save(Mockito.any(Contact.class)))
+			.thenReturn(null);
+
+		mockMvc.perform(post("/contacts/")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(contact)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+				.andExpect(jsonPath("$..details").value(hasItem(containsString("lastname"))));
+		
+		contact.setLastname("asdadh2");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("lastname"))));
+		
+		contact.setLastname("  ");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("lastname"))));
+	
+	}
+
+	@Test
+	public void postRequestForAddingContactShouldReturn400IfFullnameIsNullOrInvalid() throws Exception {
+		
+		Contact contact = new Contact();
+		contact.setFirstname("sadina");
+		contact.setLastname("sadina");
+		contact.setFullname(null);
+		contact.setEmail("saba@saba.com");
+		contact.setPhoneNumber("6219036402");
+		contact.setAddressLine("uipstrasse 0, 91524 fmgqjcat");
+		Mockito.when(contactRepository.save(Mockito.any(Contact.class)))
+			.thenReturn(null);
+
+		mockMvc.perform(post("/contacts/")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(contact)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+				.andExpect(jsonPath("$..details").value(hasItem(containsString("fullname"))));
+		
+		contact.setFullname("asdadh2");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("fullname"))));
+		
+		contact.setFirstname("  ");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("fullname"))));
+		
+	}
+	
+	@Test
+	public void postRequestForAddingContactShouldReturn400IfEmailIsNullOrInvalid() throws Exception {
+		
+		Contact contact = new Contact();
+		contact.setFirstname("sadina");
+		contact.setLastname("sadina");
+		contact.setFullname("sadina, sadina");
+		contact.setEmail(null);
+		contact.setPhoneNumber("6219036402");
+		contact.setAddressLine("uipstrasse 0, 91524 fmgqjcat");
+		Mockito.when(contactRepository.save(Mockito.any(Contact.class)))
+			.thenReturn(null);
+
+		mockMvc.perform(post("/contacts/")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(contact)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+				.andExpect(jsonPath("$..details").value(hasItem(containsString("email"))));
+		
+		contact.setEmail("asdadh2");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("email"))));
+		
+		contact.setFirstname("  ");
+		
+		mockMvc.perform(post("/contacts/")
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(contact)))
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$..message").value(hasItem("Validation Error")))
+		.andExpect(jsonPath("$..details").value(hasItem(containsString("email"))));
+		
+	}
+	
+	@Test
+	public void putRequestForUpdatingContactShouldReturn403UserNotSameAsContact() throws Exception {
+		// user id and contact id are not the same id;
+		Long contactId= 0L; 
+		Long userId= 1L;
+	
+		Contact mockContact = Mockito.mock(Contact.class,Mockito.RETURNS_DEEP_STUBS);
+		Mockito.when(mockContact.getId()).thenReturn(Long.valueOf(contactId));
+		Optional<Contact> optContact =  Optional.of(mockContact);
+		
+		Mockito.when(contactRepository.findByEmail(Mockito.any(String.class))).thenReturn(optContact);
+		
+		mockMvc.perform(put("/contacts/"+userId)
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json(getValidContact())))
+				.andExpect(status().isForbidden());	
+	}
+	
+	@Test
+	public void putRequestForUpdatingContactShouldReturn404IfUserNotFoundByEmail() throws Exception {
+		Long contactId = 1L;
+		
+		Optional<Contact> optContact =  Optional.ofNullable(null);
+		Mockito.when(contactRepository.findByEmail(Mockito.any(String.class))).thenReturn(optContact);
+		mockMvc.perform(put("/contacts/"+contactId)
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(getValidContact())))
+		.andExpect(status().isNotFound());
+		
+	}
+
+	@Test
+	public void putRequestForUpdatingContactShouldReturn400IfContactNotValid() throws Exception {
+		Long contactId = 1L;
+		
+		Optional<Contact> optContact =  Optional.ofNullable(null);
+		Mockito.when(contactRepository.findByEmail(Mockito.any(String.class))).thenReturn(optContact);
+		mockMvc.perform(put("/contacts/"+contactId)
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(getInvalidContact())))
+		.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void putRequestForUpdatingContactShouldReturn201IfContactNotValid() throws Exception {
+		Long contactId = 1L;
+		
+		Optional<Contact> optContact =  Optional.ofNullable(null);
+		Mockito.when(contactRepository.findByEmail(Mockito.any(String.class))).thenReturn(optContact);
+		mockMvc.perform(put("/contacts/"+contactId)
+				.with(SecurityMockMvcRequestPostProcessors.csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json(getInvalidContact())))
+		.andExpect(status().isBadRequest());
+	}
+	
+	
+	protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
+    }
 }
